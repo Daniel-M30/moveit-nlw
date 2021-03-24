9f0bfe474a6f030bb83cbeae382180500d06c0aa
Daniel-M30 <daniel_monteiro30@hotmail.com>
2021-03-24 10:23:01 -0300

    Update repository

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/Daniel-M30.png" alt="Daniel"/>
+            <div>
+                <strong>Daniel Monteiro</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
